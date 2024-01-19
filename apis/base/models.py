1eedc9abc3841abfa66b18bb959e26b2ade0096f
VISHNU <79331573+casio17@users.noreply.github.com>
2024-01-19 09:37:00 +0530

    1stcommit

--- apis/base/models.py
+++ apis/base/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
+    creat = models.DateTimeField(auto_now_add=True)
 
